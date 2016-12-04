3951979cfdcd4398d8cd0bcd9edccb9abcbf3851
Steven van Klei <stevenvanklei@gmail.com>
2016-12-04 17:00:16 +0100

    * Added 2016.2.2

--- 2016.Day02/Program.cs
+++ 2016.Day02/Program.cs
@@ -12,41 +12,60 @@ namespace _2016.sDay02
         static void Main(string[] args)
         {
             string input = File.ReadAllText("input.txt");
-            //input = "ULL\n" + //#1 TEST: Should show 1985
+            //input = "ULL\n" + //#1 TEST: Should show 1985 #2: 5DB3
             //        "RRDDD\n" +
             //        "LURDL\n" +
             //        "UUUUD";
 
-            int posX = 1;
-            int posY = 1;
-            string[,] keypad = new string[,] {
+            string[,] keypad1 = new string[,] {
                 { "1", "2", "3" },
                 { "4", "5", "6" },
                 { "7", "8", "9" }};
 
+            string[,] keypad2 = new string[,] {
+                { null, null, "1", null, null },
+                { null, "2",  "3", "4",  null },
+                { "5",  "6",  "7", "8",  "9"  },
+                { null, "A",  "B", "C",  null },
+                { null, null, "D", null, null }};
+
             string[] lines = input.Split('\n');
 
+            string code1 = GetCode(keypad1, lines, 1, 1);
+            string code2 = GetCode(keypad2, lines, 0, 2);
+
+            Console.WriteLine("Code #1 is: " + code1);
+            Console.WriteLine("Code #2 is: " + code2);
+            Console.ReadKey();
+        }
+
+        private static string GetCode(string[,] keypad, string[] lines, int posX, int posY)
+        {
             int keypadWidth = keypad.GetLength(1);
             int keypadHeight = keypad.GetLength(0);
 
-            string code = lines
+            return lines
                 .Aggregate("", (codeSoFar, line) =>
                 {
                     line.ToList().ForEach(c =>
                     {
-                        switch(c)
+                        switch (c)
                         {
                             case 'U':
-                                posY += posY >= 1 ? -1 : 0; 
+                                posY += posY >= 1 ? -1 : 0;
+                                posY = keypad[posY, posX] != null ? posY : posY + 1;
                                 break;
                             case 'R':
                                 posX += posX < (keypadWidth - 1) ? 1 : 0;
+                                posX = keypad[posY, posX] != null ? posX : posX - 1;
                                 break;
                             case 'D':
                                 posY += posY < (keypadHeight - 1) ? 1 : 0;
+                                posY = keypad[posY, posX] != null ? posY : posY - 1;
                                 break;
                             case 'L':
                                 posX += posX >= 1 ? -1 : 0;
+                                posX = keypad[posY, posX] != null ? posX : posX + 1;
                                 break;
                         }
                     });
@@ -55,9 +74,6 @@ namespace _2016.sDay02
 
                     return codeSoFar + nextCodeNumber;
                 });
-
-            Console.WriteLine("Code is: " + code);
-            Console.ReadKey();
         }
     }
 }
