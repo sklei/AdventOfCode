3fd53b16c8613951273f48bf8b24880a9f450223
Steven van Klei <stevenvanklei@gmail.com>
2016-12-10 22:28:24 +0100

    * Added 2016.6.1

---
+++ 2016.Day06/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2016.Day06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            //input = File.ReadAllLines("input.example.txt"); //#1 easter
+
+            var hiddenMsg = input
+                .SelectMany(s => s
+                    .Select((c,i) => 
+                        new { C = c, I = i } 
+                    )
+                )
+                .GroupBy(grp => grp.I)
+                .Select(grp => grp.Select(x => x.C).ToArray())
+                .Select(v => v.GroupBy(c => c).OrderByDescending(c => c.Count()).First().Key)
+                .Aggregate("", (was, add) => was += add);
+
+            Console.WriteLine($"#1 Hidden message: {hiddenMsg}");
+            Console.ReadKey();
+        }
+    }
+}
