6e9ee147855ebbe052287dc48b1283f5c36cc3f5
Steven van Klei <stevenvanklei@gmail.com>
2016-12-10 22:31:31 +0100

    * Added 2016.6.2

--- 2016.Day06/Program.cs
+++ 2016.Day06/Program.cs
@@ -12,20 +12,32 @@ namespace _2016.Day06
         static void Main(string[] args)
         {
             string[] input = File.ReadAllLines("input.txt");
-            //input = File.ReadAllLines("input.example.txt"); //#1 easter
+            //input = File.ReadAllLines("input.example.txt"); //#1 easter #2 advent
 
-            var hiddenMsg = input
+            var hiddenMsg1 = input
                 .SelectMany(s => s
                     .Select((c,i) => 
                         new { C = c, I = i } 
                     )
                 )
                 .GroupBy(grp => grp.I)
-                .Select(grp => grp.Select(x => x.C).ToArray())
+                .Select(grp => grp.Select(x => x.C))
                 .Select(v => v.GroupBy(c => c).OrderByDescending(c => c.Count()).First().Key)
                 .Aggregate("", (was, add) => was += add);
 
-            Console.WriteLine($"#1 Hidden message: {hiddenMsg}");
+            var hiddenMsg2 = input
+                .SelectMany(s => s
+                    .Select((c, i) =>
+                        new { C = c, I = i }
+                    )
+                )
+                .GroupBy(grp => grp.I)
+                .Select(grp => grp.Select(x => x.C))
+                .Select(v => v.GroupBy(c => c).OrderBy(c => c.Count()).First().Key)
+                .Aggregate("", (was, add) => was += add);
+
+            Console.WriteLine($"#1 Hidden message: {hiddenMsg1}");
+            Console.WriteLine($"#2 Hidden message: {hiddenMsg2}");
             Console.ReadKey();
         }
     }
